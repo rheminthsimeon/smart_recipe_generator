828760cabc47de04a128296585d36b6154965278
rheminthsimeon <rheminthsimeon@gmail.com>
2025-10-17 13:07:54 +0530

    UPDATED constants.ts

--- constants.ts
+++ constants.ts
@@ -2,7 +2,6 @@ import { DietaryPreference, Difficulty } from './types';
 
 export const DIETARY_PREFERENCES: DietaryPreference[] = ['Vegetarian', 'Vegan', 'Gluten-Free', 'Dairy-Free'];
 export const DIFFICULTY_LEVELS: Difficulty[] = ['Any', 'Easy', 'Medium', 'Hard'];
-export const COOKING_TIMES = [15, 30, 45, 60, 90, 120];
 
 export const AVAILABLE_INGREDIENTS: string[] = [
     'beans', 'beef', 'broth', 'butter', 'cabbage', 'cheese', 'chickpeas',
